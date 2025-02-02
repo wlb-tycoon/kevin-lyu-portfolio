c5a6ce6557e139391d7d3890f8b8e84da3525a3c
wlb-tycoon <lovinhood1015@gmail.com>
2025-02-02 23:50:55 +0900

    feat: Add determinating of document

--- app/components/helper/glow-card.jsx
+++ app/components/helper/glow-card.jsx
@@ -4,6 +4,8 @@ import { useEffect } from "react";
 
 const GlowCard = ({ children, identifier }) => {
   useEffect(() => {
+    if (typeof window === "undefined") return;
+
     const CONTAINER = document.querySelector(`.glow-container-${identifier}`);
     const CARDS = document.querySelectorAll(`.glow-card-${identifier}`);
 
