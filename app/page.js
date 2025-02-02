af22a5aa354dceaa9f3849523513fc709de5469b
wlb-tycoon <lovinhood1015@gmail.com>
2025-02-03 00:51:43 +0900

    feat: upcomment Projects section

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default async function Home() {
       <AboutSection />
       {/* <Experience /> */}
       <Skills />
-      {/* <Projects /> */}
+      <Projects />
       {/* <Education /> */}
       {/* <ContactSection /> */}
     </div>
