f46b1c5fc964c8be201edb7e17bebe53ddd990b7
wlb-tycoon <lovinhood1015@gmail.com>
2025-02-03 00:58:44 +0900

    feat: uncomment Experience section

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default async function Home() {
     <div>
       <HeroSection />
       <AboutSection />
-      {/* <Experience /> */}
+      <Experience />
       <Skills />
       <Projects />
       {/* <Education /> */}
