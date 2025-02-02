5dd5d48952cb3a5b2416254fa732eb6201b5e1e9
wlb-tycoon <lovinhood1015@gmail.com>
2025-02-03 01:12:51 +0900

    feat: add key

--- app/components/homepage/experience/index.jsx
+++ app/components/homepage/experience/index.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { BsPersonWorkspace } from "react-icons/bs";
 import experience from "../../../assets/lottie/code.json";
 import AnimationLottie from "../../helper/animation-lottie";
-import GlowCard from "../../helper/glow-card";
+// import GlowCard from "../../helper/glow-card";
 
 function Experience() {
   return (
@@ -42,38 +42,36 @@ function Experience() {
           <div>
             <div className="flex flex-col gap-6">
               {experiences.map((experience) => (
-                <GlowCard
-                  key={experience.id}
-                  identifier={`experience-${experience.id}`}
-                >
-                  <div className="p-3 relative">
-                    <Image
-                      src="/blur-23.svg"
-                      alt="Hero"
-                      width={1080}
-                      height={200}
-                      className="absolute bottom-0 opacity-80"
-                    />
-                    <div className="flex justify-center">
-                      <p className="text-xs sm:text-sm text-[#16f2b3]">
-                        {experience.duration}
-                      </p>
+                // <GlowCard
+                //   key={experience.id}
+                //   identifier={`experience-${experience.id}`}
+                // >
+                <div className="p-3 relative" key={experience.id}>
+                  <Image
+                    src="/blur-23.svg"
+                    alt="Hero"
+                    width={1080}
+                    height={200}
+                    className="absolute bottom-0 opacity-80"
+                  />
+                  <div className="flex justify-center">
+                    <p className="text-xs sm:text-sm text-[#16f2b3]">
+                      {experience.duration}
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-x-8 px-3 py-5">
+                    <div className="text-violet-500  transition-all duration-300 hover:scale-125">
+                      <BsPersonWorkspace size={36} />
                     </div>
-                    <div className="flex items-center gap-x-8 px-3 py-5">
-                      <div className="text-violet-500  transition-all duration-300 hover:scale-125">
-                        <BsPersonWorkspace size={36} />
-                      </div>
-                      <div>
-                        <p className="text-base sm:text-xl mb-2 font-medium uppercase">
-                          {experience.company}
-                        </p>
-                        <p className="text-sm sm:text-base">
-                          {experience.title}
-                        </p>
-                      </div>
+                    <div>
+                      <p className="text-base sm:text-xl mb-2 font-medium uppercase">
+                        {experience.company}
+                      </p>
+                      <p className="text-sm sm:text-base">{experience.title}</p>
                     </div>
                   </div>
-                </GlowCard>
+                </div>
+                // </GlowCard>
               ))}
             </div>
           </div>
